e51672a111fb3a6a71ef5c329ecb207c34f2a96b
Ankan005 <Ankan005@gmail.com>
2019-12-04 13:27:29 +0000

    bringup aosip

--- aosip.mk
+++ aosip.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := aosip_rolex
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
